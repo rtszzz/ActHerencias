fec226f5768abfa04fbbf36f7e2f1d306bcdc385
usuario <usuario@LENOVO-PC>
2021-10-17 22:10:00 -0500

    Agregar archivos de proyecto.

---
+++ ActHerencias/Alumno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActHerencias
+{
+    class Alumno
+    {
+        private string Matricula;
+        private string Carrera;
+        public string matricula
+        {
+            get
+            {
+                return Matricula;
+            }
+            set
+            {
+                matricula = value;
+            }
+        }
+
+        public string carrera
+        {
+            get
+            {
+                return carrera;
+            }
+            set
+            {
+                carrera = value;
+            }
+        }
+
+        public Alumno()
+        {
+            matricula = "";
+            carrera = "";
+        }
+
+        public string (string matricula, string carrera)
+        {
+            this.matricula = matricula;
+            this.carrera = carrera;
+        }
+       
+        public override string ToString()
+        {
+        return base.ToString() + "matricula" + matricula + "carrera[" + carrera.ToString() + "]";
+        }
+    }
+}
---
+++ ActHerencias/Persona.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ActHerencias
+{
+    class Persona:Alumno
+    {
+        static void Main(string[] args)
+        {
+           private string Nombre;
+        private string Fechadenacimiento;
+
+        public string nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string fechadenacimiento
+        {
+            get
+            {
+                return fechadenacimiento;
+            }
+            set
+            {
+                fechadenacimiento = value;
+            }
+
+        }
+
+        public Persona()
+        {
+            nombre = "";
+            fechadenacimiento = "";
+        }
+
+        public Persona(string nombre, string fechadenacimiento);
+        {
+           this.nombre = nombre;
+           this.fechadenacimiento = fechadenacimiento;
+        }
+
+       public override string ToString()
+       {
+        return. base ToString() + "nombre" + nombre + "fechadenacimiento[" + fechadenacimiento.ToString() + "]";
+       }
+    }
+}
+
+
+
+
